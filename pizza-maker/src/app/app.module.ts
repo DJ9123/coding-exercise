898766b5831328efcb18efb111868e9503b601cf
DJ <dj9123@live.com>
2023-02-16 09:25:16 -0600

    Add base styles and content placeholders

--- pizza-maker/src/app/app.module.ts
+++ pizza-maker/src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Modules
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './modules/material.module';
+
+// Components
+import { AppComponent } from './app.component';
 import { NavToolbarComponent } from './components/nav-toolbar/nav-toolbar.component';
 import { LoginComponent } from './pages/login/login.component';
 import { OrdersComponent } from './pages/orders/orders.component';
@@ -20,7 +24,8 @@ import { HomeComponent } from './pages/home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
