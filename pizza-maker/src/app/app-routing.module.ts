e4369455c48f978fa29077ec481da7456173ccbf
DJ <dj9123@live.com>
2023-02-16 09:11:57 -0600

    Add basic routing with basic auth guard

--- pizza-maker/src/app/app-routing.module.ts
+++ pizza-maker/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { OrdersComponent } from './pages/orders/orders.component';
+import { AuthGuard } from './guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    canActivateChild: [AuthGuard],
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'orders', component: OrdersComponent },
+    ],
+  },
+  { path: 'login', component: LoginComponent },
+  
+  { path: '**', redirectTo: '' }, // redirect to home
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- pizza-maker/src/app/app.module.ts
+++ pizza-maker/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavToolbarComponent } from './components/nav-toolbar/nav-toolbar.component';
+import { LoginComponent } from './pages/login/login.component';
+import { OrdersComponent } from './pages/orders/orders.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavToolbarComponent,
+    LoginComponent,
+    OrdersComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
